a2758ab03fe697590c04191722dfaa8d3a681794
RealMarkSussex <marksussex6@gmail.com>
2020-04-28 22:04:08 +0100

    added cards and more tunes

--- frontend/src/views/Beats.vue
+++ frontend/src/views/Beats.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
+  <div class="container">
     <div>
       <SearchBar @searchBeats="filterBeats" />
     </div>
     <br />
     <div>
       <BeatList :beats="filteredBeats" />
+    </div>
+    <div>
+      <div>
+        <b-card bg-variant="dark" text-variant="white" title="MP3 Licence">
+          <b-card-text>Some Shite about MP3 features</b-card-text>
+          <b-button href="#" variant="primary">Get Licence</b-button>
+        </b-card>
+      </div>
+      <br />
+
+      <div>
+        <b-card bg-variant="dark" text-variant="white" title="WAV Licence">
+          <b-card-text>Some Shite about WAV features content.</b-card-text>
+          <b-button href="#" variant="primary">Get Licence</b-button>
+        </b-card>
+      </div>
+      <br />
+
+      <div>
+        <b-card bg-variant="dark" text-variant="white" title="Premium Licence">
+          <b-card-text>Some Shite about premiem features</b-card-text>
+          <b-button href="#" variant="primary">Get License</b-button>
+        </b-card>
+      </div>
     </div>
   </div>
 </template>
@@ -56,6 +80,26 @@ export default {
             { id: 1, name: "Uk Drill" },
             { id: 2, name: "Uk Afros" }
           ]
+        },
+        {
+          id: 5,
+          title: "Rap God",
+          time: "2:10",
+          BPM: 50,
+          tags: [
+            { id: 3, name: "Pop" },
+            { id: 4, name: "Rap" }
+          ]
+        },
+        {
+          id: 6,
+          title: "Cold",
+          time: "3:30",
+          BPM: 0,
+          tags: [
+            { id: 4, name: "Rap" },
+            { id: 5, name: "Hip" }
+          ]
         }
       ],
       filteredBeats: []
@@ -83,10 +127,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.guitar {
-  width: 100%;
-  height: 100%;
-}
-</style>
