7405d3b53f73b30ae03837b398cb7763e2ffece5
RealMarkSussex <marksussex6@gmail.com>
2020-05-05 21:34:57 +0100

    added google login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,13 +5,62 @@
         <router-link to="/" class="nav">Beats</router-link>
         <router-link to="/Contact" class="nav">Contact</router-link>
         <router-link to="/Beats" class="nav">Beats</router-link>
+        <GoogleLogin
+          :params="params"
+          :onSuccess="onSuccess"
+          :onFailure="onFailure"
+          >Login</GoogleLogin
+        >
+        <GoogleLogin
+          :params="params"
+          :logoutButton="true"
+          class="btn btn-danger"
+          >Logout</GoogleLogin
+        >
       </b-navbar-nav>
     </b-navbar>
+    <p>Logged in with: {{ this.email }}</p>
     <br />
     <router-view />
   </div>
 </template>
+<script>
+import GoogleLogin from "vue-google-login";
 
+export default {
+  name: "App",
+  data() {
+    return {
+      // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+      params: {
+        client_id: "951491493515-fj3umvhhdeo0p0ua93874qaarnrn82td"
+      },
+      // only needed if you want to render the button with the google ui
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true
+      },
+      email: ""
+    };
+  },
+  components: {
+    GoogleLogin
+  },
+  methods: {
+    onSuccess(googleUser) {
+      console.log(googleUser);
+      let profile = googleUser.getBasicProfile();
+      // This only gets the user information: id, name, imageUrl and email
+      console.log(profile);
+      this.email = profile.yu;
+    },
+    onFailure() {
+      alert("Failed to login!");
+    }
+  }
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
