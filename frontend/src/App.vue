f8f3072fc20095bfb331bbc26b1b47a4bfc2efc4
RealMarkSussex <marksussex6@gmail.com>
2020-05-06 21:59:30 +0100

    removed dist

---
+++ frontend/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <router-link to="/" class="nav">Home</router-link>
+        <router-link to="/Contact" class="nav">Contact</router-link>
+        <router-link to="/Beats" class="nav">Beats</router-link>
+        <GoogleLogin
+          :params="params"
+          :renderParams="renderParams"
+          :onSuccess="onSuccess"
+          :onFailure="onFailure"
+        ></GoogleLogin>
+        <GoogleLogin
+          :params="params"
+          :logoutButton="true"
+          class="btn btn-danger"
+          >Logout</GoogleLogin
+        >
+      </b-navbar-nav>
+    </b-navbar>
+    <br />
+    <router-view />
+  </div>
+</template>
+<script>
+import GoogleLogin from "vue-google-login";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+      params: {
+        client_id: "951491493515-fj3umvhhdeo0p0ua93874qaarnrn82td"
+      },
+      // only needed if you want to render the button with the google ui
+      renderParams: {
+        width: 250,
+        height: 50,
+        longtitle: true
+      },
+      email: ""
+    };
+  },
+  components: {
+    GoogleLogin
+  },
+  methods: {
+    onSuccess(googleUser) {
+      console.log(googleUser);
+      let profile = googleUser.getBasicProfile();
+      // This only gets the user information: id, name, imageUrl and email
+      console.log(profile.getEmail());
+      this.email = profile.getEmail();
+    },
+    onFailure() {
+      alert("Failed to Login!");
+    }
+  }
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #418bc8;
+  height: 100vh;
+}
+
+.nav {
+  padding: 10px;
+  font-weight: bold;
+  color: #418bc8;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,78 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col cols="6">
+        <FeaturedBeats :beats="beats" />
+      </b-col>
+      <b-col cols="6">
+        <b-row>
+          <b-col>
+            <b-card bg-variant="dark" text-variant="white" title="MP3 Licence">
+              <b-card-text>Some Shite about MP3 features</b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-card bg-variant="dark" text-variant="white" title="WAV Licence">
+              <b-card-text>Some Shite about WAV features</b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-card
+              bg-variant="dark"
+              text-variant="white"
+              title="Premium Licence"
+            >
+              <b-card-text>Some Shite about premiem features</b-card-text>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+    <br />
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import FeaturedBeats from "@/components/FeaturedBeats.vue";
+export default {
+  name: "Home",
+  components: {
+    FeaturedBeats
+  },
+  data() {
+    return {
+      beats: [
+        {
+          id: 1,
+          title: "Spider",
+          time: "2:33",
+          BPM: 0,
+          tags: [
+            { id: 1, name: "Uk Drill" },
+            { id: 2, name: "Uk Afros" }
+          ]
+        },
+        {
+          id: 2,
+          title: "Hamster",
+          time: "3:20",
+          BPM: 0,
+          tags: [{ id: 2, name: "Uk Afros" }]
+        },
+        {
+          id: 3,
+          title: "Sicko Mode",
+          time: "1:40",
+          BPM: 5,
+          tags: [{ id: 2, name: "Uk Afros" }]
+        }
+      ]
+    };
+  }
+};
+</script>
